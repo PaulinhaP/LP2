bb422aa0d4a9a63de92d3400ce5ca2f24068128c
PaulinhaP <62159871+PaulinhaP@users.noreply.github.com>
2020-05-15 22:59:00 -0300

    Add files via upload

---
+++ Atividade 4/Salario/Salario/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace Salario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbnFeminino = new System.Windows.Forms.RadioButton();
+            this.rbnMasculino = new System.Windows.Forms.RadioButton();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtFilhos = new System.Windows.Forms.TextBox();
+            this.ckbCasado = new System.Windows.Forms.CheckBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtAliquotaINSS = new System.Windows.Forms.TextBox();
+            this.txtAliquotaIRPF = new System.Windows.Forms.TextBox();
+            this.txtSalFamilia = new System.Windows.Forms.TextBox();
+            this.txtSalLiquido = new System.Windows.Forms.TextBox();
+            this.txtDescINSS = new System.Windows.Forms.TextBox();
+            this.txtDescIRPF = new System.Windows.Forms.TextBox();
+            this.mskSalario = new System.Windows.Forms.MaskedTextBox();
+            this.lblDados = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Salário Bruto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número de Filhos:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbnMasculino);
+            this.groupBox1.Controls.Add(this.rbnFeminino);
+            this.groupBox1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(385, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(113, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Genêro";
+            // 
+            // rbnFeminino
+            // 
+            this.rbnFeminino.AutoSize = true;
+            this.rbnFeminino.Location = new System.Drawing.Point(19, 28);
+            this.rbnFeminino.Name = "rbnFeminino";
+            this.rbnFeminino.Size = new System.Drawing.Size(82, 22);
+            this.rbnFeminino.TabIndex = 0;
+            this.rbnFeminino.TabStop = true;
+            this.rbnFeminino.Text = "Feminino";
+            this.rbnFeminino.UseVisualStyleBackColor = true;
+            // 
+            // rbnMasculino
+            // 
+            this.rbnMasculino.AutoSize = true;
+            this.rbnMasculino.Location = new System.Drawing.Point(19, 61);
+            this.rbnMasculino.Name = "rbnMasculino";
+            this.rbnMasculino.Size = new System.Drawing.Size(88, 22);
+            this.rbnMasculino.TabIndex = 1;
+            this.rbnMasculino.TabStop = true;
+            this.rbnMasculino.Text = "Masculino";
+            this.rbnMasculino.UseVisualStyleBackColor = true;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(155, 71);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 25);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtFilhos
+            // 
+            this.txtFilhos.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFilhos.Location = new System.Drawing.Point(155, 151);
+            this.txtFilhos.Name = "txtFilhos";
+            this.txtFilhos.Size = new System.Drawing.Size(100, 25);
+            this.txtFilhos.TabIndex = 6;
+            // 
+            // ckbCasado
+            // 
+            this.ckbCasado.AutoSize = true;
+            this.ckbCasado.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbCasado.Location = new System.Drawing.Point(404, 177);
+            this.ckbCasado.Name = "ckbCasado";
+            this.ckbCasado.Size = new System.Drawing.Size(71, 22);
+            this.ckbCasado.TabIndex = 7;
+            this.ckbCasado.Text = "Casado";
+            this.ckbCasado.UseVisualStyleBackColor = true;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(238, 200);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(107, 27);
+            this.btnVerificar.TabIndex = 8;
+            this.btnVerificar.Text = "Verificar Desconto";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Alíquota INSS:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 343);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Alíquota IRPF:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Salário Família:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 423);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 18);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Salário Líquido:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(359, 303);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 18);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Desconto INSS:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(359, 343);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 18);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Desconto IRPF:";
+            // 
+            // txtAliquotaINSS
+            // 
+            this.txtAliquotaINSS.Enabled = false;
+            this.txtAliquotaINSS.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAliquotaINSS.Location = new System.Drawing.Point(155, 300);
+            this.txtAliquotaINSS.Name = "txtAliquotaINSS";
+            this.txtAliquotaINSS.Size = new System.Drawing.Size(100, 25);
+            this.txtAliquotaINSS.TabIndex = 15;
+            // 
+            // txtAliquotaIRPF
+            // 
+            this.txtAliquotaIRPF.Enabled = false;
+            this.txtAliquotaIRPF.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAliquotaIRPF.Location = new System.Drawing.Point(155, 340);
+            this.txtAliquotaIRPF.Name = "txtAliquotaIRPF";
+            this.txtAliquotaIRPF.Size = new System.Drawing.Size(100, 25);
+            this.txtAliquotaIRPF.TabIndex = 16;
+            // 
+            // txtSalFamilia
+            // 
+            this.txtSalFamilia.Enabled = false;
+            this.txtSalFamilia.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalFamilia.Location = new System.Drawing.Point(155, 380);
+            this.txtSalFamilia.Name = "txtSalFamilia";
+            this.txtSalFamilia.Size = new System.Drawing.Size(100, 25);
+            this.txtSalFamilia.TabIndex = 17;
+            // 
+            // txtSalLiquido
+            // 
+            this.txtSalLiquido.Enabled = false;
+            this.txtSalLiquido.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalLiquido.Location = new System.Drawing.Point(155, 420);
+            this.txtSalLiquido.Name = "txtSalLiquido";
+            this.txtSalLiquido.Size = new System.Drawing.Size(100, 25);
+            this.txtSalLiquido.TabIndex = 18;
+            // 
+            // txtDescINSS
+            // 
+            this.txtDescINSS.Enabled = false;
+            this.txtDescINSS.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescINSS.Location = new System.Drawing.Point(471, 300);
+            this.txtDescINSS.Name = "txtDescINSS";
+            this.txtDescINSS.Size = new System.Drawing.Size(100, 25);
+            this.txtDescINSS.TabIndex = 19;
+            // 
+            // txtDescIRPF
+            // 
+            this.txtDescIRPF.Enabled = false;
+            this.txtDescIRPF.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescIRPF.Location = new System.Drawing.Point(471, 340);
+            this.txtDescIRPF.Name = "txtDescIRPF";
+            this.txtDescIRPF.Size = new System.Drawing.Size(100, 25);
+            this.txtDescIRPF.TabIndex = 20;
+            // 
+            // mskSalario
+            // 
+            this.mskSalario.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mskSalario.Location = new System.Drawing.Point(155, 111);
+            this.mskSalario.Mask = "000,000.00";
+            this.mskSalario.Name = "mskSalario";
+            this.mskSalario.Size = new System.Drawing.Size(100, 25);
+            this.mskSalario.TabIndex = 21;
+            // 
+            // lblDados
+            // 
+            this.lblDados.AutoSize = true;
+            this.lblDados.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDados.Location = new System.Drawing.Point(260, 245);
+            this.lblDados.Name = "lblDados";
+            this.lblDados.Size = new System.Drawing.Size(0, 18);
+            this.lblDados.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(179, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(219, 26);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Desconto Salário Bruto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 473);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblDados);
+            this.Controls.Add(this.mskSalario);
+            this.Controls.Add(this.txtDescIRPF);
+            this.Controls.Add(this.txtDescINSS);
+            this.Controls.Add(this.txtSalLiquido);
+            this.Controls.Add(this.txtSalFamilia);
+            this.Controls.Add(this.txtAliquotaIRPF);
+            this.Controls.Add(this.txtAliquotaINSS);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.ckbCasado);
+            this.Controls.Add(this.txtFilhos);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbnMasculino;
+        private System.Windows.Forms.RadioButton rbnFeminino;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtFilhos;
+        private System.Windows.Forms.CheckBox ckbCasado;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtAliquotaINSS;
+        private System.Windows.Forms.TextBox txtAliquotaIRPF;
+        private System.Windows.Forms.TextBox txtSalFamilia;
+        private System.Windows.Forms.TextBox txtSalLiquido;
+        private System.Windows.Forms.TextBox txtDescINSS;
+        private System.Windows.Forms.TextBox txtDescIRPF;
+        private System.Windows.Forms.MaskedTextBox mskSalario;
+        private System.Windows.Forms.Label lblDados;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Atividade 4/Salario/Salario/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Salario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double descontoINSS = 0;
+            double descontoIRPF = 0;
+            double salarioLiquido = 0;
+            double salarioBruto = 0;
+            double salarioFamilia = 0;
+            double numeroFilhos = 0;
+
+            if (txtNome.Text == String.Empty)
+            {
+                MessageBox.Show("Nome não pode estar vazio!", "Informações Incompletas", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+                if(!double.TryParse(mskSalario.Text, out salarioBruto) 
+                    || !double.TryParse(txtFilhos.Text, out numeroFilhos))
+                {
+                    MessageBox.Show("Salário bruto e número de filhos devem ser númericos!", "Informações Incompletas", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    //INSS
+                    if(salarioBruto <= 1045)
+                    {
+                        txtAliquotaINSS.Text = "7,5%";
+                        descontoINSS = 0.075 * salarioBruto;
+                    }
+                    else if (salarioBruto <= 2089.6)
+                    {
+                        txtAliquotaINSS.Text = "9%";
+                        descontoINSS = 0.09 * salarioBruto;
+                    }
+                    else if (salarioBruto <= 3134.4)
+                    {
+                        txtAliquotaINSS.Text = "12%";
+                        descontoINSS = 0.12 * salarioBruto;
+                    }
+                    else
+                    {
+                        txtAliquotaINSS.Text = "14%";
+                        descontoINSS = 0.14 * salarioBruto;
+                    }
+
+                    txtDescINSS.Text = descontoINSS.ToString("N2");
+
+                    //IRPF
+                    if (salarioBruto <= 1903.98)
+                        txtAliquotaIRPF.Text = "0";
+
+                    else if (salarioBruto <= 2826.65)
+                    {
+                        txtAliquotaIRPF.Text = "7.5%";
+                        descontoIRPF = (salarioBruto * 7.5 / 100);
+                    }
+                    else if (salarioBruto <= 3751.05)
+                    {
+                        txtAliquotaIRPF.Text = "15%";
+                        descontoIRPF = (salarioBruto * 15 / 100);
+                    }
+                    else if (salarioBruto <= 4664.68)
+                    {
+                        txtAliquotaIRPF.Text = "22.5%";
+                        descontoIRPF = (salarioBruto * 22.5 / 100);
+                    }
+                    else
+                    {
+                        txtAliquotaIRPF.Text = "27.5";
+                        descontoIRPF = ((27.5 / 100) * salarioBruto);
+                    }
+
+                    txtDescIRPF.Text = descontoIRPF.ToString("N2");
+
+                    //SALÁRIO FAMÍLIA
+                    if(numeroFilhos > 0)
+                    {
+                        if (salarioBruto <= 435.52)
+                            salarioFamilia = (22.33 * numeroFilhos);
+                        else if (salarioBruto <= 654.61)
+                            salarioFamilia = (15.74 * numeroFilhos);
+                        else
+                            salarioFamilia = 0;
+                    }
+                    salarioLiquido = salarioBruto - descontoINSS - descontoIRPF + salarioFamilia;
+                    txtSalFamilia.Text = salarioFamilia.ToString("N2");
+                    txtSalLiquido.Text = salarioLiquido.ToString("N2");
+
+                    //variável = condição ? se verdadeiro : se falso
+                    lblDados.Text = "Os descontos do salário " + (rbnFeminino.Checked ? "da Sra. " : "do Sr. ") + 
+                        txtNome.Text + "\n" + " que é " + (ckbCasado.Checked ? "Casado(a)" : "Solteiro(a)") + 
+                        " e que tem " + Convert.ToString(numeroFilhos) + " filho(s) são:";
+                }
+
+        }
+    }
+}

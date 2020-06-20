d865e4ad6cadf2c25f379c6938bf5dfd1d59ba09
PaulinhaP <62159871+PaulinhaP@users.noreply.github.com>
2020-06-19 23:00:26 -0300

    Add files via upload

---
+++ Prova/P0030481923047/P0030481923047/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace P0030481923047
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lbxTexto = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(74, 93);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lbxTexto
+            // 
+            this.lbxTexto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbxTexto.FormattingEnabled = true;
+            this.lbxTexto.Location = new System.Drawing.Point(255, 12);
+            this.lbxTexto.Name = "lbxTexto";
+            this.lbxTexto.Size = new System.Drawing.Size(174, 238);
+            this.lbxTexto.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 262);
+            this.Controls.Add(this.lbxTexto);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Prova";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lbxTexto;
+    }
+}
+
---
+++ Prova/P0030481923047/P0030481923047/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030481923047
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] Matriz = new double[7, 4];
+            double valor, Total = 0;
+            int mes = 0, sem = 0;
+            string valorSem = "";
+
+
+
+            while (mes < 7)
+            {
+                while (sem < 4)
+                {
+                    valorSem = Interaction.InputBox("Digite o valor total vendido na semana " + (sem + 1) + " do mês " + (mes + 1) + ": ", "Entrada de Dados");
+                    if (valorSem == "")
+                    {
+                        break;
+                    }
+                    else if (!double.TryParse(valorSem, out valor))
+                    {
+                        MessageBox.Show("Digite um valor válido!");
+                        sem--;
+                    }
+                    else
+                    {
+                        valor = Convert.ToDouble(valorSem);
+                        Matriz[mes, sem] = valor;
+                    }
+                    sem++;
+                }
+                if (valorSem == "")
+                {
+                    break;
+                }
+                sem = 0;
+                mes++;
+            }
+            sem = 0;
+            mes = 0;
+
+            while (mes < 7)
+            {
+                while (sem < 4)
+                {
+
+                    lbxTexto.Items.Add("Mês " + (mes + 1) + ", semana " + (sem + 1) + ": R$ " + (Matriz[mes, sem].ToString("N2")));
+                    Total = Total + Matriz[mes, sem];
+                    sem++;
+                }
+
+                lbxTexto.Items.Add("Total do mês " + (mes + 1) + ": R$ " + ((Matriz[mes, 0] + Matriz[mes, 1] + Matriz[mes, 2] + Matriz[mes, 3]).ToString("N2")));
+                lbxTexto.Items.Add("*************************************");
+
+                sem = 0;
+                mes++;
+            }
+            lbxTexto.Items.Add("Total final: R$ " + (Total.ToString("N2")));
+        }
+    }
+}
